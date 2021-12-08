6a258cb7de3cebf5c8ca39cfdea80f876329a309
james <junyoungcat@gmail.com>
2021-12-08 22:25:46 +0900

    Create test line

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('renders tasks', () => {
+    const { container } = render((
+      <App/>
+    ));
+
+    expect(container).toHaveTextContent('아무 일도 하기 싫다.');
+  })
+})
