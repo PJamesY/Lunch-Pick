e1a72660ecd6b698dfda9da9ec91e20edebd6ae5
james <junyoungcat@gmail.com>
2021-12-10 22:47:07 +0900

    Create List component

---
+++ src/List.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function List() {
+  return (
+    <div className="List">
+      <h1>TO - Do</h1>
+      <ul>
+        <li>
+          아무 일도 하기 싫다.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
