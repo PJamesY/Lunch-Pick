9e35f36f0e73a9e2f5ba04b2b8a1f721c32df684
james <junyoungcat@gmail.com>
2021-12-10 22:45:24 +0900

    Change to List component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
+import List from './List';
+
 describe('App', () => {
   it('renders tasks', () => {
     const { container } = render((
-      <App/>
+      <List />
     ));
 
     expect(container).toHaveTextContent('아무 일도 하기 싫다.');
