0ed7c26a503cf70af7341205e721b5499b71debf
james <junyoungcat@gmail.com>
2021-12-11 22:20:34 +0900

    Create List test

--- src/List.jsx
+++ src/List.jsx
@@ -3,7 +3,6 @@ import './App.css';
 function List({ tasks }) {
   return (
     <div className="List">
-      <h1>TO - Do</h1>
       <ul>
           {tasks.map((task) => 
               <li key={task.id}>
