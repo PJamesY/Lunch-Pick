99b6ce7b522df06bde021be43725b67f324f8a56
james <junyoungcat@gmail.com>
2021-12-11 22:20:00 +0900

    Import List Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
+import List from './List';
+
 function App() {
+  const tasks = [
+    { id: 1, title: '아무 일도 하기 싫다'},
+    { id: 2, title: 'TDD 연습'},
+  ]
   return (
     <div className="App">
       <h1>TO - Do</h1>
-      <ul>
-        <li>
-          아무 일도 하기 싫다.
-        </li>
-      </ul>
+      <List tasks={tasks} />
     </div>
   );
 }
