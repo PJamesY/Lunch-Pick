0ca50654722d169e6fb2c6ae84e88d78137798d8
james <junyoungcat@gmail.com>
2021-12-14 22:43:06 +0900

    Apply fixer task

--- src/List.test.js
+++ src/List.test.js
@@ -1,18 +1,15 @@
 import { render } from '@testing-library/react';
 import List from './List';
+import tasks from '../fixer/tasks'
 
 describe('List', () => {
   it('renders tasks', () => {
-    const tasks = [
-      { id: 1, title: '아무 일도 하기 싫다'},
-      { id: 2, title: 'TDD 연습'},
-    ]
+
     const { container } = render((
       <List tasks={tasks} />
     ));
 
     expect(container).toHaveTextContent('아무 일도 하기 싫다');
     expect(container).toHaveTextContent('TDD 연습');
-
   })
 })
