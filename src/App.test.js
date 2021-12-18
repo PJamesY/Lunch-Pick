e601401b3978c471dd21a8019564b6374a1c4d61
james <junyoungcat@gmail.com>
2021-12-18 15:27:44 +0900

    Add ts config

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,23 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  // useSelector.mockImplementation((selector) => selector({
+  //   tasks: [
+  //     { id: 1, title: '아무 일도 하기 싫다'},
+  //     { id: 2, title: 'TDD 연습'},
+  //   ],
+  // }));
+  
   it('renders tasks', () => {
+    
     const { container } = render((
-      <App />
+<App />
+      
     ));
 
-    expect(container).toHaveTextContent('아무 일도 하기 싫다');
+    expect(container).toHaveTextContent('TO');
   })
 })
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-
-import reducer from './reducer';
-
-const store = createStore(reducer);
-
-export default store;
