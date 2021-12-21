841290f56d8d1ef3bf5c93ed5ecb5b2937d2ebf4
james <junyoungcat@gmail.com>
2021-12-21 22:23:37 +0900

    Create navigation component

---
+++ src/header/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>메인</li>
+        <li>소개</li>
+        <li>포트폴리오</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
