d564f570b0e270985f457f691b1842398f6304b6
james <junyoungcat@gmail.com>
2021-12-25 23:04:47 +0900

    Apply styled component

--- src/header/Navigation.tsx
+++ src/header/Navigation.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-scroll';
+
+const StyledLi = styled.li`
+  display: inline-block;
+  &.active {
+    color: red;
+  }
+`;
+
+StyledLi.displayName = 'li';
+
+const StyledNavUl = styled.ul``;
 
 const Navigation = function Navigation() {
+  const [tab, setTab] = useState('main');
+
+  const changeTab = () => {
+    setTab('메인');
+  };
+
   return (
     <nav>
       <ul>
-        <li>메인</li>
-        <li>소개</li>
-        <li>포트폴리오</li>
+        <StyledLi onClick={changeTab}>
+          <Link onClick={changeTab} to="home" smooth activeClass="active">
+            메인
+          </Link>
+        </StyledLi>
+        <StyledLi>
+          <Link to="work" smooth activeClass="active">
+            포트폴리오
+          </Link>
+        </StyledLi>
+        <StyledLi>
+          <Link to="profile">메인</Link>
+        </StyledLi>
       </ul>
     </nav>
   );
