59ca19f0ee555dfccc69ab09fac33d4e336f59e5
james <junyoungcat@gmail.com>
2021-12-26 23:03:17 +0900

    Add change color test

--- src/header/Navigation.test.tsx
+++ src/header/Navigation.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ShallowWrapper, shallow } from 'enzyme';
-import Navigation from './Navigation.tsx';
+import Navigation from './Navigation';
 
 let wrapper: ShallowWrapper;
 
 beforeEach(() => {
-  wrapper = shallow(<Navigation />);
+  wrapper = shallow(<Navigation tab="" />);
 });
 
 describe('Navigation', () => {
-  it('have three contents', () => {
-    const expectedTexts = ['메인', '소개', '포트폴리오'];
-    const renderdTexts = expectedTexts.map((val, idx) => wrapper.find('li').at(idx).text());
-    expect(renderdTexts).toEqual(expectedTexts);
+  it('should change color when clicked', () => {
+    expect(wrapper.find('li').at(0).props().color).toBe('white');
+    wrapper.find('li').at(0).childAt(0).simulate('click');
+    expect(wrapper.find('li').at(0).props().color).toBe('purple');
   });
 });
