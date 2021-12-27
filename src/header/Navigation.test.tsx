49db30eac2ffabf5f7b28fad8d61d2bd0cf44234
james <junyoungcat@gmail.com>
2021-12-27 22:52:17 +0900

    Test other 3 tag

--- src/header/Navigation.test.tsx
+++ src/header/Navigation.test.tsx
@@ -5,13 +5,31 @@ import Navigation from './Navigation';
 let wrapper: ShallowWrapper;
 
 beforeEach(() => {
-  wrapper = shallow(<Navigation tab="" />);
+  wrapper = shallow(<Navigation defaultTab="" />);
 });
 
 describe('Navigation', () => {
-  it('should change color when clicked', () => {
-    expect(wrapper.find('li').at(0).props().color).toBe('white');
-    wrapper.find('li').at(0).childAt(0).simulate('click');
-    expect(wrapper.find('li').at(0).props().color).toBe('purple');
+  describe('click li tag', () => {
+    it('should change color when click first li', () => {
+      expect(wrapper.find('li').at(0).props().color).toBe('white');
+      wrapper.find('li').at(0).childAt(0).simulate('click');
+      expect(wrapper.find('li').at(0).props().color).toBe('purple');
+      expect(wrapper.find('li').at(1).props().color).toBe('white');
+      expect(wrapper.find('li').at(2).props().color).toBe('white');
+    });
+    it('should change color when click second li', () => {
+      expect(wrapper.find('li').at(1).props().color).toBe('white');
+      wrapper.find('li').at(1).childAt(0).simulate('click');
+      expect(wrapper.find('li').at(0).props().color).toBe('white');
+      expect(wrapper.find('li').at(1).props().color).toBe('purple');
+      expect(wrapper.find('li').at(2).props().color).toBe('white');
+    });
+    it('should change color when click third li', () => {
+      expect(wrapper.find('li').at(2).props().color).toBe('white');
+      wrapper.find('li').at(2).childAt(0).simulate('click');
+      expect(wrapper.find('li').at(0).props().color).toBe('white');
+      expect(wrapper.find('li').at(1).props().color).toBe('white');
+      expect(wrapper.find('li').at(2).props().color).toBe('purple');
+    });
   });
 });
