3ee1aeba2a98f91143dcf265344e7d8ab22bd350
james <junyoungcat@gmail.com>
2021-12-28 22:42:24 +0900

    Add sections component

---
+++ src/sections/Sections.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+import Work from './Work';
+
+const Sections = function Sections() {
+  return (
+    <>
+      <Home />
+      <Work />
+    </>
+  );
+};
+
+export default Sections;
