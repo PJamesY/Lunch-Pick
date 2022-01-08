78d4e827de2595f77b7dec291d2bb9885b7667d0
james <junyoungcat@gmail.com>
2022-01-08 23:09:20 +0900

    Add project

---
+++ src/sections/Project.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledSection } from '../styledComponent/Section';
+
+const Project = function Project() {
+  return <StyledSection id="project">Project</StyledSection>;
+};
+
+export default Project;
--- src/sections/Sections.tsx
+++ src/sections/Sections.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './Home';
 import Work from './Work';
+import Project from './Project';
 
 const Sections = function Sections() {
   return (
     <>
       <Home />
       <Work />
+      <Project />
     </>
   );
 };
