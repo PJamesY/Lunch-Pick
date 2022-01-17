5cd77b2e3c50de0ac84ebbef86e0adf7be36fccc
james <junyoungcat@gmail.com>
2022-01-17 22:49:01 +0900

    Test click menu button

--- src/header/Navigation.test.tsx
+++ src/header/Navigation.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ShallowWrapper, shallow } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Navigation from './Navigation';
 
 let wrapper: ShallowWrapper;
@@ -22,7 +23,7 @@ describe('Navigation', () => {
     it('should change color when click first li', () => {
       expect(wrapper.find('li').at(0).props().color).toBe('white');
       wrapper.find('li').at(0).childAt(0).simulate('click');
-      expect(wrapper.find('li').at(0).props().color).toBe('purple');
+      expect(wrapper.find('li').at(0).props().color).toBe('red');
       expect(wrapper.find('li').at(1).props().color).toBe('white');
       expect(wrapper.find('li').at(2).props().color).toBe('white');
     });
@@ -30,7 +31,7 @@ describe('Navigation', () => {
       expect(wrapper.find('li').at(1).props().color).toBe('white');
       wrapper.find('li').at(1).childAt(0).simulate('click');
       expect(wrapper.find('li').at(0).props().color).toBe('white');
-      expect(wrapper.find('li').at(1).props().color).toBe('purple');
+      expect(wrapper.find('li').at(1).props().color).toBe('red');
       expect(wrapper.find('li').at(2).props().color).toBe('white');
     });
     it('should change color when click third li', () => {
@@ -38,7 +39,25 @@ describe('Navigation', () => {
       wrapper.find('li').at(2).childAt(0).simulate('click');
       expect(wrapper.find('li').at(0).props().color).toBe('white');
       expect(wrapper.find('li').at(1).props().color).toBe('white');
-      expect(wrapper.find('li').at(2).props().color).toBe('purple');
+      expect(wrapper.find('li').at(2).props().color).toBe('red');
+    });
+  });
+
+  describe('click menu button', () => {
+    it('should show close button when click menu button', () => {
+      render(
+        <Navigation
+          defaultTab=""
+          menus={[
+            { key: 'home', to: 'home', name: '메인' },
+            { key: 'work', to: 'work', name: '포트폴리오' },
+            { key: 'project', to: 'project', name: '프로젝트' },
+          ]}
+        />,
+      );
+      const dialogContainer = screen.getByLabelText('menu');
+      fireEvent.click(dialogContainer);
+      expect(screen.getByLabelText('close')).toBeInTheDocument();
     });
   });
 });
