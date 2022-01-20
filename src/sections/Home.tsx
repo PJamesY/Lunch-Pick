1f1e780c6412a846c22370b388e594ab9751e780
james <junyoungcat@gmail.com>
2022-01-20 22:57:55 +0900

    Add StyledImg

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { StyledSection } from '../styledComponent/Section';
+import hello from '../asset/hello.gif';
+
+const StyledImg = styled.img`
+  width: 100%;
+`;
 
 const Home = function Home() {
-  return <StyledSection id="home">home</StyledSection>;
+  return (
+    <StyledSection id="home">
+      <StyledImg src={hello} alt="loading..." />
+    </StyledSection>
+  );
 };
 
 export default Home;
