dcaa8eb20de8a3973f35e961e2094377ff63d669
james <junyoungcat@gmail.com>
2022-01-24 22:56:46 +0900

    Display none when mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import useScroll from './hooks/useScroll';
 import Header from './header/Header';
 
 import Sections from './sections/Sections';
-import hello from './asset/hello.gif';
+import castle from './asset/castle.gif';
 
 const StyledImg = styled.div<{ animate: number }>`
   // width: 100%;
@@ -14,10 +14,16 @@ const StyledImg = styled.div<{ animate: number }>`
   // background-attachment: fixed;
   // background-size: 100% 40%;
   // height: 100%;
-  background-image: url(https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375);
+  // background-image: url(https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375);
+  background: url(${castle});
+
   background-attachment: fixed;
-  background-size: 100% 40%;
-  height: 300px;
+  background-size: cover;
+  height: 600px;
+
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
   // height: 1000px;
   // background-image: ;
   // margin-top: ${({ animate }) => (animate > 0 ? animate : 0)}px;
