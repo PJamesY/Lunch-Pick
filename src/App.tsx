3ee3faba1e0f2097d5ab32d92b623fc7d8f69cf0
james <junyoungcat@gmail.com>
2022-01-25 23:00:25 +0900

    Assign first background image

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,17 @@ import useScroll from './hooks/useScroll';
 import Header from './header/Header';
 
 import Sections from './sections/Sections';
-import castle from './asset/castle.gif';
 
 const StyledImg = styled.div<{ animate: number }>`
-  // width: 100%;
-  // height: ${({ animate }) => 500 - animate}px;
-  // background-image: url(https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375);
-  // background-attachment: fixed;
-  // background-size: 100% 40%;
-  // height: 100%;
-  // background-image: url(https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375);
-  background: url(${castle});
-
+  background-image: url('https://images.unsplash.com/photo-1449452198679-05c7fd30f416?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375');
   background-attachment: fixed;
-  background-size: cover;
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100vw;
   height: 600px;
-
   @media screen and (max-width: 500px) {
     display: none;
   }
-  // height: 1000px;
-  // background-image: ;
-  // margin-top: ${({ animate }) => (animate > 0 ? animate : 0)}px;
 `;
 
 const App = function App(): ReactElement {
