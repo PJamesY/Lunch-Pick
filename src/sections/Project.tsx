da5225bf7b35c0bdb773540d420e5a489ccefcce
james <junyoungcat@gmail.com>
2022-01-29 15:38:26 +0900

    Add display flex

--- src/sections/Project.tsx
+++ src/sections/Project.tsx
@@ -21,15 +21,46 @@ const StyledCell = styled.div`
   border-radius: 50%;
   width: 60px;
   height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledPopup = styled.div`
+  position: absolute;
+  top: 35%;
+  left: 22%;
+  width: 400px;
+  height: 200px;
+  background-color: orange;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Project = function Project() {
+  const showPopup = (msg: string) => {
+    console.log('click', msg);
+  };
+
   return (
     <StyledSection id="project">
       <StyledBoard>
         <StyledRow>
-          <StyledCell>1</StyledCell>
-          <StyledCell>2</StyledCell>
+          <StyledCell
+            onClick={() => {
+              showPopup('암송');
+            }}
+          >
+            1
+          </StyledCell>
+          <StyledCell
+            onClick={() => {
+              showPopup('암송');
+            }}
+          >
+            2
+          </StyledCell>
           <StyledCell>3</StyledCell>
           <StyledCell>4</StyledCell>
           <StyledCell>5</StyledCell>
@@ -54,6 +85,7 @@ const Project = function Project() {
           <StyledCell>5</StyledCell>
         </StyledRow>
       </StyledBoard>
+      <StyledPopup>jjjj</StyledPopup>
     </StyledSection>
   );
 };
