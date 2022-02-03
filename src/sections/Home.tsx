48275d7c3a559cc223a0e35e3c6ceab8bcb0a415
james <junyoungcat@gmail.com>
2022-02-03 23:02:31 +0900

    Delete image

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { StyledSection } from '../styledComponent/Section';
-import hello from '../asset/hello.gif';
-
-const StyledImg = styled.img`
-  width: 100%;
-`;
 
 const Home = function Home() {
-  return (
-    <StyledSection id="home">
-      <StyledImg src={hello} alt="background" />
-    </StyledSection>
-  );
+  return <StyledSection id="home">Home</StyledSection>;
 };
 
 export default Home;
