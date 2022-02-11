d932372768e602e98f5268704ec3c2addcec98c2
james <junyoungcat@gmail.com>
2022-02-11 22:55:53 +0900

    Add Query client

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,4 +1,7 @@
 import React, { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const Pokemon = function Pokemon(): ReactElement {
   return <div>pokemon</div>;
