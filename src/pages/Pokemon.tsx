60e9ea0debf018c81512bb31c9ab2a973ac5de60
james <junyoungcat@gmail.com>
2022-02-12 23:12:36 +0900

    Add example component

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -3,6 +3,10 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 const queryClient = new QueryClient();
 
+const Example = function Example() {
+  return <div>example</div>;
+};
+
 const Pokemon = function Pokemon(): ReactElement {
   return <div>pokemon</div>;
 };
