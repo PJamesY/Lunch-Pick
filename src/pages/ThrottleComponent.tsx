d287f0e0975c08d5092ed93943b5ee5b4ea58660
james <junyoungcat@gmail.com>
2022-02-13 19:25:16 +0900

    Add throttle func

--- src/pages/ThrottleComponent.tsx
+++ src/pages/ThrottleComponent.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { throttle } from 'lodash';
 
 const ThrottleComponent = function ThrottleComponent() {
-  return <div>throttle</div>;
+  const [value, setValue] = useState(0);
+  const throttled = useCallback(
+    throttle((newValue) => console.log(newValue), 10000),
+    [],
+  );
+
+  useEffect(() => {
+    throttled(value);
+  }, [value]);
+
+  return (
+    <button onClick={() => setValue(value + 1)} type="button">
+      {value}
+    </button>
+  );
 };
 
 export default ThrottleComponent;
