2925807f14476d6dfeeb5bbbcf6562a574c2be5d
james <junyoungcat@gmail.com>
2022-02-13 18:41:46 +0900

    Create throttle practice component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Pokemon from './pages/Pokemon';
 import useScroll from './hooks/useScroll';
 import Header from './header/Header';
 import Sections from './sections/Sections';
+import ThrottleComponent from './pages/ThrottleComponent';
 
 const StyledImg = styled.div<{ animate: number }>`
   background-image: url('https://images.unsplash.com/photo-1449452198679-05c7fd30f416?crop=entropy&fit=crop&fm=jpg&h=650&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375');
@@ -25,6 +26,8 @@ const App = function App(): ReactElement {
     <div className="App">
       <Routes>
         <Route path="/pokemon" element={<Pokemon />} />
+        <Route path="/throttle" element={<ThrottleComponent />} />
+
         <Route
           path="/"
           element={
