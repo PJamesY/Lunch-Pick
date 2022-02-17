1d7ca86f33f2ffc45f2b12e7d1be725cc53e90b8
james <junyoungcat@gmail.com>
2022-02-17 23:05:49 +0900

    Add head tag

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const App = function App() {
+  //   const { scrollY } = useScroll();
+
+  return (
+    <div className="App">
+      <Head>
+        <title>Project</title>
+      </Head>
+    </div>
+  );
+};
+
+export default App;
