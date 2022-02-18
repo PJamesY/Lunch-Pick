a28ec836323da9957b3a8ed7f40fc30065e2b6e9
james <junyoungcat@gmail.com>
2022-02-18 23:03:42 +0900

    Import navbar into _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
