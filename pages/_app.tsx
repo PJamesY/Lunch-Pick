16bb2f361f52dfd7f4ddc9eb7a8a4696a3103e12
james <junyoungcat@gmail.com>
2022-02-19 20:39:09 +0900

    Create global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 import NavBar from '../components/NavBar';
+import { useRouter } from 'next/router';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <>
-      <NavBar />
+      {router.pathname !== '/flower' && <NavBar />}
       <Component {...pageProps} />
     </>
   );
