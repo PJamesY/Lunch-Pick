5a72a8ae483a29da7f250bd5d2804755bb9ae9f3
james <junyoungcat@gmail.com>
2022-02-23 23:05:01 +0900

    Add profile image

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,18 @@
+import { AiOutlineUser } from 'react-icons/ai';
+
 const NavBar = function NavBar() {
   return (
     <>
       <div className="nav-bar">
         <span className="title">JAMES</span>
+        <AiOutlineUser />
       </div>
       <style jsx>
         {`
           .nav-bar {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
             background-color: #f76707;
           }
           .title {
