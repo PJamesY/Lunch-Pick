c13b6db7ccd432abc0ce63e9572ab26cdbf450d3
james <junyoungcat@gmail.com>
2022-03-13 22:49:31 +0900

    Define restArea api

--- server/routes/restArea.js
+++ server/routes/restArea.js
@@ -21,14 +21,27 @@ const getRestAreaList = async () => {
   }
 };
 
-const getAreaRestFood = async () => {
+const getAreaRestFood = async (code) => {
   try {
     response = await axios.get(
       `${BEST_FOOD_API_URL}?${encodeURIComponent("key")}=${
         process.env.REST_AREA_RECOMMEND_FOOD_KEY
       }&${encodeURIComponent("type")}=json&${encodeURIComponent(
-        "numOfRows"
-      )}=20&${encodeURIComponent("stdRestCd")}=000001`
+        "stdRestCd"
+      )}=${code}`
+    );
+    return response;
+  } catch (e) {
+    // error
+  }
+};
+
+const getRestArea = async (code) => {
+  try {
+    response = await axios.get(
+      `${REST_AREA_LIST_URL}?${encodeURIComponent("key")}=${
+        process.env.REST_AREA_RECOMMEND_FOOD_KEY
+      }&${encodeURIComponent("type")}=json&svarCd=${code}`
     );
     return response;
   } catch (e) {
@@ -37,19 +50,24 @@ const getAreaRestFood = async () => {
 };
 
 router.get("/food", (req, res) => {
-  //   console.log("req");
-  getAreaRestFood().then((response) => {
+  console.log("req", req.query.id);
+  getAreaRestFood(req.query.id).then((response) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    console.log("rer", response);
-    res.send({ error: false, data: response.data });
+    res.send({ data: response.data.list });
   });
 });
 
 router.get("/restAreaList", (req, res) => {
   getRestAreaList().then((response) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    console.log("asdfasdfasdfsdfas", response.list);
     res.send({ data: response.list });
+  });
+});
+
+router.get("/restArea", (req, res) => {
+  getRestArea(req.query.id).then((response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send({ data: response.data.list[0] });
   });
 });
 
