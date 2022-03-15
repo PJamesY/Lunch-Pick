740ede6d580ffc8095de6a432bad594794186968
james <junyoungcat@gmail.com>
2022-03-15 23:34:49 +0900

    Delete test code

--- client/src/sections/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Home from './Home';
-
-describe('Home section', () => {
-  it('should match snapShot', () => {
-    expect(shallow(<Home />)).toMatchSnapshot();
-  });
-});
