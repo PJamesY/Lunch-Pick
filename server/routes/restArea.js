527b703811327d5b35f8b237e4d42c0caa24d3df
james <junyoungcat@gmail.com>
2022-03-24 23:42:02 +0900

    Get rest area location

--- server/routes/restArea.js
+++ server/routes/restArea.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const axios = require("axios");
+const { response } = require("express");
 require("dotenv").config();
 
 const REST_AREA_LIST_URL =
@@ -7,6 +8,9 @@ const REST_AREA_LIST_URL =
 
 const BEST_FOOD_API_URL =
   "http://data.ex.co.kr/openapi/restinfo/restBestfoodList";
+
+const REST_AREA_LIST_LOC =
+  "http://data.ex.co.kr/openapi/locationinfo/locationinfoRest";
 
 const getRestAreaList = async () => {
   try {
@@ -49,6 +53,54 @@ const getRestArea = async (code) => {
   }
 };
 
+const getRestAreaLoc = async () => {
+  try {
+    let arr = [];
+    const response = await axios.get(
+      `${REST_AREA_LIST_LOC}?${encodeURIComponent("key")}=${
+        process.env.REST_AREA_RECOMMEND_FOOD_KEY
+      }&${encodeURIComponent("type")}=json&${encodeURIComponent(
+        "numOfRows"
+      )}=100&${encodeURIComponent("pageNo")}=1`
+    );
+    const response2 = await axios.get(
+      `${REST_AREA_LIST_LOC}?${encodeURIComponent("key")}=${
+        process.env.REST_AREA_RECOMMEND_FOOD_KEY
+      }&${encodeURIComponent("type")}=json&${encodeURIComponent(
+        "numOfRows"
+      )}=100&${encodeURIComponent("pageNo")}=2`
+    );
+    const response3 = await axios.get(
+      `${REST_AREA_LIST_LOC}?${encodeURIComponent("key")}=${
+        process.env.REST_AREA_RECOMMEND_FOOD_KEY
+      }&${encodeURIComponent("type")}=json&${encodeURIComponent(
+        "numOfRows"
+      )}=100&${encodeURIComponent("pageNo")}=3`
+    );
+    arr = [
+      ...response.data.list,
+      ...response2.data.list,
+      ...response3.data.list,
+    ];
+    console.log(arr.length);
+    return arr;
+    // arr = [...arr, ...response.data.list];
+    // // for (let i = 1; i < 4; i++) {
+
+    // // }
+    // console.log("arr", arr);
+    // return arr;
+    // response = await axios.get(
+    //   `${REST_AREA_LIST_LOC}?${encodeURIComponent("key")}=${
+    //     process.env.REST_AREA_RECOMMEND_FOOD_KEY
+    //   }&${encodeURIComponent("type")}=json&${encodeURIComponent(
+    //     "numOfRows"
+    //   )}=100&${encodeURIComponent("pageNo")}=1`
+    // );
+    // return response;
+  } catch (e) {}
+};
+
 router.get("/food", (req, res) => {
   console.log("req", req.query.id);
   getAreaRestFood(req.query.id).then((response) => {
@@ -60,6 +112,7 @@ router.get("/food", (req, res) => {
 router.get("/restAreaList", (req, res) => {
   getRestAreaList().then((response) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(response.list);
     res.send({ data: response.list });
   });
 });
@@ -68,6 +121,13 @@ router.get("/restArea", (req, res) => {
   getRestArea(req.query.id).then((response) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.send({ data: response.data.list[0] });
+  });
+});
+
+router.get("/restAreaLoc", (req, res) => {
+  getRestAreaLoc().then((response) => {
+    // console.log(response.data.list);
+    res.send({ data: response });
   });
 });
 
