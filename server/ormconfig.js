81ac21feb6c7e496977815205e519f35e64441ff
james <junyoungcat@gmail.com>
2022-04-13 22:35:20 +0900

    Add ormconfig

---
+++ server/ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { RestAreaModule } from './rest-area/rest-area.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, RestAreaModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, RestAreaModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
