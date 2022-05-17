1ad4a650b5e777830e26b7e45c6f96be56cb3517
james <junyoungcat@gmail.com>
2022-05-17 22:48:38 +0900

    Change api path

--- client/api/restAreaList.ts
+++ client/api/restAreaList.ts
@@ -2,11 +2,13 @@ import axios from 'axios';
 import { IRestAreaList } from '../types/restArea';
 
 async function getRestAreaList() {
-  const restAreaList = await axios.get('http://localhost:5000/api/restAreaList');
+  const restAreaList = await axios.get('http://localhost:5000/rest-area/list');
 
-  const restAreaListWithoutGasStation = restAreaList.data.data.filter((elem) => elem.svarNm.slice(-3) === '휴게소');
+  const restAreaListWithoutGasStation = restAreaList.data.data.filter(
+    (elem: any) => elem.svarNm.slice(-3) === '휴게소',
+  );
 
-  const filteredData: IRestAreaList = restAreaListWithoutGasStation.map((elem) => ({
+  const filteredData: IRestAreaList = restAreaListWithoutGasStation.map((elem: any) => ({
     routeCode: elem.routeCd,
     routeName: elem.routeNm,
     restAreaName: elem.svarNm,
