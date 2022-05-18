27e1f529fa2516a2860d83e4f3058956563f7008
james <junyoungcat@gmail.com>
2022-05-18 22:59:12 +0900

    Add rest area type

--- client/api/restAreaList.ts
+++ client/api/restAreaList.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
-import { IRestAreaList } from '../types/restArea';
+import { IExtractedRestArea, IRestArea } from '../types/restArea';
 
 async function getRestAreaList() {
   const restAreaList = await axios.get('http://localhost:5000/rest-area/list');
-
-  const restAreaListWithoutGasStation = restAreaList.data.data.filter(
-    (elem: any) => elem.svarNm.slice(-3) === '휴게소',
+  const restAreaListWithoutGasStation = restAreaList.data.list.filter(
+    (elem: IRestArea) => elem.svarNm.slice(-3) === '휴게소',
   );
 
-  const filteredData: IRestAreaList = restAreaListWithoutGasStation.map((elem: any) => ({
+  const filteredData: IExtractedRestArea = restAreaListWithoutGasStation.map((elem: IRestArea) => ({
     routeCode: elem.routeCd,
     routeName: elem.routeNm,
     restAreaName: elem.svarNm,
--- client/types/restArea.ts
+++ client/types/restArea.ts
@@ -1,7 +1,29 @@
-export interface IRestAreaList {
+export interface IExtractedRestArea {
   routeCode: string;
   routeName: string;
   restAreaName: string;
   restAreaCode: string;
   directionCode: '0' | '1';
+}
+
+export interface IRestArea {
+  routeCd: string;
+  svarAddr: string;
+  routeNm: string;
+  svarCd: string;
+  svarNm: string;
+  hdqrCd: string;
+  hdqrNm: string;
+  mtnofCd: string;
+  mtnofNm: string;
+  svarGsstClssCd: string;
+  svarGsstClssNm: string;
+  gudClssCd: string;
+  gudClssNm: string;
+  pstnoCd: string;
+  cocrPrkgTrcn: string;
+  fscarPrkgTrcn: string;
+  dspnPrkgTrcn: string;
+  bsopAdtnlFcltCd: string;
+  rprsTelNo: string;
 }
