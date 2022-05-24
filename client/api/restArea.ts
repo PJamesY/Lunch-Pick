27fedaabbe32b98498e4e7cf85972dd723d99728
james <junyoungcat@gmail.com>
2022-05-24 22:53:56 +0900

    Chang api name

--- client/api/restArea.ts
+++ client/api/restArea.ts
@@ -2,14 +2,13 @@ import axios from 'axios';
 import { IRestAreaList } from '../types/restArea';
 
 async function getRestArea(code: string) {
-  const response = await axios.get('http://localhost:5000/api/restArea', { params: { id: code } });
-  console.log('res', response.data.data);
+  const response = await axios.get('http://localhost:5000/rest-area', { params: { id: code } });
   const restArea = {
-    routeCode: response.data.data.routeCd,
-    routeName: response.data.data.routeNm,
-    restAreaName: response.data.data.svarNm,
-    restAreaCode: response.data.data.svarCd,
-    directionCode: response.data.data.gudClssCd,
+    routeCode: response.data.routeCd,
+    routeName: response.data.routeNm,
+    restAreaName: response.data.svarNm,
+    restAreaCode: response.data.svarCd,
+    directionCode: response.data.gudClssCd,
   };
 
   return restArea;
