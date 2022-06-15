3bd8e9c47c1d78cf191990885ab5a857b3253742
james <junyoungcat@gmail.com>
2022-06-15 22:38:45 +0900

    Add Food interface type

--- client/types/restArea.ts
+++ client/types/restArea.ts
@@ -27,3 +27,36 @@ export interface IRestArea {
   bsopAdtnlFcltCd: string;
   rprsTelNo: string;
 }
+
+export interface IExtractedFood {
+  foodName: string;
+  isRecommend: 'N' | 'Y';
+  isBest: 'N' | 'Y';
+  isPremium: 'N' | 'Y';
+  price: string;
+}
+
+export interface IFood {
+  pageNo: number | null;
+  numOfRows: number | null;
+  stdRestCd: string;
+  stdRestNm: string;
+  lsttmAltrUser: string;
+  lsttmAltrDttm: string;
+  svarAddr: string;
+  routeCd: string;
+  routeNm: string;
+  seq: string;
+  foodNm: string;
+  foodCost: string;
+  etc: string;
+  recommendyn: 'Y' | 'N';
+  seasonMenu: '4';
+  bestfoodyn: 'Y' | 'N';
+  premiumyn: 'Y' | 'N';
+  app: 'Y' | 'N';
+  restCd: string;
+  foodMaterial: string | null;
+  lastId: string;
+  lastDtime: string;
+}
